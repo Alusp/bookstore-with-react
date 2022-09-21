338cf1427c7fa7cff70881920996a981ae8733cd
Aseni <alusinejalloh800@gmail.com>
2022-09-21 21:44:35 +0300

    Update `BookList.js` files

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 
 function BookList() {
+  const allBooks = useSelector((state) => state.booksReducer);
   return (
     <div className="flex flex-col">
-      <Book title="Book 1" />
-      <Book title="Book 2" />
-      <Book title="Book 3" />
-      <Book title="Book 4" />
-      <Book title="Book 5" />
+      {
+        allBooks.length ? (
+          allBooks.map((book) => (
+            <Book
+              id={book.id}
+              key={book.id}
+              title={book.title}
+              author={book.author}
+            />
+          ))
+        ) : (
+          <div>
+            <h2 className="text-xl font-semibold">No Books</h2>
+          </div>
+        )
+      }
+
     </div>
   );
 }
