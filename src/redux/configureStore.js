d751520e8a2cfa6a8e4dae5931e4387df74257bd
Aseni <alusinejalloh800@gmail.com>
2022-09-21 21:36:03 +0300

    Add `configure.js` files

---
+++ src/redux/configureStore.js
@@ -0,0 +1,10 @@
+import { createStore, combineReducers } from 'redux';
+import booksReducer from './books/books';
+
+const reducer = combineReducers({
+  booksReducer,
+});
+
+const store = createStore(reducer);
+
+export default store;
