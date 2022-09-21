acb2e6f76142a6353f53b2308b4036a82e6e78c1
Aseni <alusinejalloh800@gmail.com>
2022-09-21 21:44:06 +0300

    Update `Book.js` files

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removedBook } from '../redux/books/books';
 
-function Book({ title, author }) {
+function Book({ title, author, id }) {
+  const dispatch = useDispatch();
+
+  const handleRemoveBook = () => {
+    dispatch(removedBook({ id }));
+  };
   return (
     <div className="my-5 border flex justify-between items-center">
       <h2 className="text-xl font-semibold">{title}</h2>
       <h2 className="text-xl font-semibold">{author}</h2>
-      <button type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <button onClick={handleRemoveBook} type="button" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Remove
       </button>
     </div>
@@ -15,6 +22,7 @@ function Book({ title, author }) {
 Book.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default Book;
