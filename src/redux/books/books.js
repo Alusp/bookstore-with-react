418c854bea591db5359600e27a6e3ad6eed1a9e9
Aseni <alusinejalloh800@gmail.com>
2022-09-23 14:06:31 +0300

    add initial array values of books

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,7 +1,35 @@
+import { v4 as uuidv4 } from 'uuid';
+
 const BOOK_REMOVED = 'bookStore/books/BOOK_REMOVED';
 const BOOK_ADDED = 'bookStore/books/BOOK_ADDED';
 
-const initialState = [];
+const initialState = [
+
+  {
+    id: uuidv4(),
+    title: 'Capital in the Twenty-First Century',
+  },
+  {
+    id: uuidv4(),
+    title: 'The Lord of the Rings',
+  },
+  {
+    id: uuidv4(),
+    title: 'The Little Prince',
+  },
+  {
+    id: uuidv4(),
+    title: 'The Hobbit',
+  },
+  {
+    id: uuidv4(),
+    title: 'And Then There Were None',
+  },
+  {
+    id: uuidv4(),
+    title: 'Dream of the Red Chamber',
+  },
+];
 export const removedBook = (payload) => (
   {
     type: BOOK_REMOVED,
