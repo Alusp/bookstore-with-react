0cc747d47933e9d1afef2236ee356e8db87a3f43
Aseni <alusinejalloh800@gmail.com>
2022-09-24 18:44:03 +0300

    Update Categories.js

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,33 +1,17 @@
-const CHECK_STATUS = 'bookStore/categories/STATUS';
-const ADD_CATEGORY = 'bookStore/categories/ADD_CATEGORY';
-const REMOVE_CATEGORY = 'bookStore/categories/REMOVE_CATEGORY';
+const CHECKER = 'bookstore/book/CHECKER';
 
-const initialState = [];
-const checkStatus = (state = initialState, action) => {
+export const CHECK = () => (
+  {
+    type: CHECKER,
+  });
+
+const catagoryReducer = (state = '', action) => {
   switch (action.type) {
-    case CHECK_STATUS:
-      return 'Under Construction';
-    case ADD_CATEGORY:
-      return [...state, action.payload];
-    case REMOVE_CATEGORY:
-      return state.filter((category) => category !== action.categoryId);
+    case CHECKER:
+      return 'UNDER DEVELOPMENT';
     default:
       return state;
   }
 };
 
-export const checkStatusAction = () => ({
-  type: CHECK_STATUS,
-});
-
-export const addCategory = (payload) => ({
-  type: ADD_CATEGORY,
-  payload,
-});
-
-export const removeCategory = (categoryId) => ({
-  type: REMOVE_CATEGORY,
-  categoryId,
-});
-
-export default checkStatus;
+export default catagoryReducer;
