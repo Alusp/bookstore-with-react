10cfe27b8c36137b58f95e421fd1b4e843018980
Aseni <alusinejalloh800@gmail.com>
2022-09-24 19:14:47 +0300

    Add index.css to show tailind css styles

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/configureStore';
 
-// import './index.css';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
